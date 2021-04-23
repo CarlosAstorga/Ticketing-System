a76b8554f65808ec0f5a72fc7b75fcae90f1aa4e
Carlos Astorga <carlosasthorga@gmail.com>
2021-04-22 20:51:19 -0500

    dashboard view

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -4,33 +4,65 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Ticketing System</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/dashboard.css') }}" rel="stylesheet">
+
 </head>
 
 <body>
+    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+        <span class="navbar-brand col-md-3 col-lg-2 me-0 px-3">T-System</span>
+        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </header>
 
-    <nav class="sidenav">
-        <a href="" class="item" title="Dashboard" data-bs-toggle="tooltip" data-bs-placement="right">
-            <i class="fas fa-chart-pie"></i>
-        </a>
-        <a href="" class="item" title="Usuarios" data-bs-toggle="tooltip" data-bs-placement="right">
-            <i class="fas fa-users"></i>
-        </a>
-        <a href="" class="item" title="Proyectos" data-bs-toggle="tooltip" data-bs-placement="right">
-            <i class="fas fa-project-diagram"></i>
-        </a>
-        <a href="" class="item" title="Tickets" data-bs-toggle="tooltip" data-bs-placement="right">
-            <i class="fas fa-tasks"></i>
-        </a>
-    </nav>
+    <div class="container-fluid">
+        <div class="row">
+            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+                <div class="position-sticky pt-3">
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">
+                                <span class="fas fa-chart-pie"></span>
+                                Dashboard
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <span class="
+                                fas fa-users"></span>
+                                Usuarios
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <span class="fas fa-project-diagram"></span>
+                                Proyectos
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <span class="fas fa-tasks"></span>
+                                Tickets
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                @yield('content')
+            </main>
+        </div>
+    </div>
 
     <script src=" {{ asset('js/app.js') }}" defer></script>
-    <script src=" {{ asset('js/tooltip.js') }}" defer></script>
 </body>
 
 </html>
